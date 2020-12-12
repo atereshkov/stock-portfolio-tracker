ec66aaa89decd08615ca4ff3f6d66766918e1fec
Alexander Tereshkov <alexander@tereshkov.pw>
2020-12-12 14:27:18 +0300

    Base SwiftUI project

---
+++ stock-portfolio-tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stock-portfolio-tracker
+//
+//  Created by nb-058-41b on 12/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stock-portfolio-tracker/stock_portfolio_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  stock_portfolio_trackerApp.swift
+//  stock-portfolio-tracker
+//
+//  Created by nb-058-41b on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct stock_portfolio_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
