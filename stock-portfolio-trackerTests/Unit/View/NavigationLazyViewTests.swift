207a8f1198c04220b596fbc113b2ec48b024424b
Alexander Tereshkov <alexander@tereshkov.pw>
2020-12-26 12:00:10 +0300

    Tests

---
+++ stock-portfolio-trackerTests/Unit/View/NavigationLazyViewTests.swift
@@ -0,0 +1,25 @@
+//
+//  NavigationLazyViewTests.swift
+//  stock-portfolio-trackerTests
+//
+//  Created by nb-058-41b on 12/26/20.
+//
+
+import XCTest
+import SwiftUI
+@testable import stock_portfolio_tracker
+
+private struct DummyView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+class NavigationLazyViewTests: XCTestCase {
+    
+    func testView() {
+//        let lazyView = NavigationLazyView(DummyView())
+//        let values = lazyView.inspect().map { try $0.text().string() }
+    }
+
+}
